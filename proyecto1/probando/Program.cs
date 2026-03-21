cccc55e02f530011efa097a4ecece9e184ce5162
adrianPoot <adrian902@hotmail.com>
2026-03-21 09:21:35 -0600

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, Platzi");
 
 
 string stripe_api_key="sdk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+
+string  testingConnection ="Server=tcp:adrianserver.database.windows.net,1433;Initial Catalog=adrianDB;Persist Security Info=False;User ID=adrian;Password=Adrian1234;MultipleActiveResultSets=False;Encrypt=True;TrustserverCertificate=False;Connection Timeout=30;"; 
